16543b6c569c5269e4c813ea13ce1b5362f1b916
James Snee <james.snee@gmail.com>
2013-07-25 15:46:25 +0100

    Added view controller and view for the historical data view

---
+++ DTG_Weather/Historical_Controller.h
@@ -0,0 +1,13 @@
+//
+//  HistoricalViewController.h
+//  DTG_Weather
+//
+//  Created by James Snee on 25/07/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoricalViewController : UIViewController
+
+@end
---
+++ DTG_Weather/Historical_Controller.m
@@ -0,0 +1,38 @@
+//
+//  HistoricalViewController.m
+//  DTG_Weather
+//
+//  Created by James Snee on 25/07/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import "HistoricalViewController.h"
+
+@interface HistoricalViewController ()
+
+@end
+
+@implementation HistoricalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
