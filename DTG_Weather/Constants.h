64b4653619430ad5926a20dc96b79edd5711722a
James Snee <james.snee@gmail.com>
2013-11-07 22:39:37 +0000

    Consolidated constants into a single header

---
+++ DTG_Weather/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  DTG_Weather
+//
+//  Created by James Snee on 07/11/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#ifndef DTG_Weather_Constants_h
+#define DTG_Weather_Constants_h
+
+#define WS_URL @"http://www.cl.cam.ac.uk/research/dtg/weather/"
+
+#endif
