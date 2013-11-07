0bf7b9a39bcd231c26cb658326f7f22106f3127e
James Snee <james.snee@gmail.com>
2013-11-07 22:35:44 +0000

    Removed old code

--- DTG_Weather/DTG_WeatherStation.m
+++ DTG_Weather/DTG_WeatherStation.m
@@ -20,7 +20,6 @@
 	}
 
 	NSArray *weather_comps = [weather_string componentsSeparatedByString:@"\n"];
-//	NSLog(@"%@",[weather_comps description]);
 
 	//Check that we actually have some weather data.
 	if([weather_comps count]<2)
@@ -33,28 +32,24 @@
 	temp_str = [[temp_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	temp_str = [temp_str stringByReplacingOccurrencesOfString:@"C" withString:@""];
 	[weather setTemp:[temp_str floatValue]];
-//	NSLog(@"%f",[weather temp]);
 	
 	//Pressure
 	NSString *pressure_str = [weather_comps objectAtIndex:3];
 	pressure_str = [[pressure_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	pressure_str = [pressure_str stringByReplacingOccurrencesOfString:@"mBar" withString:@""];
 	[weather setPressure:[pressure_str floatValue]];
-//	NSLog(@"%f",[weather pressure]);
 	
 	//Humidity
 	NSString *humidity_str = [weather_comps objectAtIndex:4];
 	humidity_str = [[humidity_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	humidity_str = [humidity_str stringByReplacingOccurrencesOfString:@"%" withString:@""];
 	[weather setHumidity:[humidity_str intValue]];
-//	NSLog(@"%d",[weather humidity]);
 	
 	//Dewpoint
 	NSString *dew_str = [weather_comps objectAtIndex:5];
 	dew_str = [[dew_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	dew_str = [dew_str stringByReplacingOccurrencesOfString:@"C" withString:@""];
 	[weather setDew_point:[dew_str floatValue]];
-//	NSLog(@"%f",[weather dew_point]);
 	
 	//Wind
 	NSString *wind_str = [weather_comps objectAtIndex:6];
@@ -62,27 +57,23 @@
 	NSArray *wind_comps = [wind_str componentsSeparatedByString:@"knotsfromthe"];
 	[weather setWind_speed:[[wind_comps objectAtIndex:0] floatValue]];
 	[weather setWind_direction:[wind_comps objectAtIndex:1]];
-//	NSLog(@"%f, %@",[weather wind_speed],[weather wind_direction]);
 	
 	//Sunshine
 	NSString *sun_str = [weather_comps objectAtIndex:7];
 	sun_str = [[sun_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	sun_str = [[sun_str componentsSeparatedByString:@"hours"] objectAtIndex:0];
 	[weather setSun_hours:[sun_str floatValue]];
-//	NSLog(@"%f",[weather sun_hours]);
 	
 	//Rainfall
 	NSString *rain_str = [weather_comps objectAtIndex:8];
 	rain_str = [[rain_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	rain_str = [[rain_str componentsSeparatedByString:@"mm"] objectAtIndex:0];
 	[weather setRain:[rain_str floatValue]];
-//	NSLog(@"%f",[weather rain]);
 	
 	//Summary
 	NSString *summary_str = [weather_comps objectAtIndex:10];
 	summary_str = [[summary_str componentsSeparatedByString:@":"] objectAtIndex:1];
 	[weather setSummary:summary_str];
-//	NSLog(@"%@",[weather summary]);
     
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
     NSCalendarUnit unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit| NSWeekCalendarUnit |NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
@@ -103,8 +94,6 @@
 }
 
 - (NSArray *) get_weatherForToday{
-	//NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-	
 	NSError *error;
 	NSString *weather_string = [NSString stringWithContentsOfURL:[NSURL URLWithString:@"http://www.cl.cam.ac.uk/research/dtg/weather/daily-text.cgi?2013-02-02"] encoding:NSUTF8StringEncoding error:&error];
 	if(error){
